895026b5a2eabb3285735afa12aa89b1dedfd308
thecrown <amienkurniawan5@gmail.com>
2017-12-03 21:55:29 +0700

    3 Des 2017

--- perhutani.sql
+++ perhutani.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 31, 2017 at 11:30 AM
+-- Generation Time: Dec 03, 2017 at 03:53 PM
 -- Server version: 10.1.16-MariaDB
 -- PHP Version: 5.5.38
 
@@ -30,6 +30,7 @@ CREATE TABLE `admin` (
   `id_admin` int(5) UNSIGNED NOT NULL,
   `username` char(30) NOT NULL,
   `nama_lengkap` varchar(30) NOT NULL,
+  `no_telephone` varchar(255) NOT NULL,
   `password` varchar(40) NOT NULL,
   `role` enum('admin','operator') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -38,9 +39,12 @@ CREATE TABLE `admin` (
 -- Dumping data for table `admin`
 --
 
-INSERT INTO `admin` (`id_admin`, `username`, `nama_lengkap`, `password`, `role`) VALUES
-(1, 'admin', 'Yohana Agustina Ginting', 'c3284d0f94606de1fd2af172aba15bf3', 'admin'),
-(2, 'operator', 'operator', 'a2ac1e2a67f66b89e4b181d542c8fcca', 'operator');
+INSERT INTO `admin` (`id_admin`, `username`, `nama_lengkap`, `no_telephone`, `password`, `role`) VALUES
+(1, 'admin', 'Yohana Agustina Ginting', '9090', 'c3284d0f94606de1fd2af172aba15bf3', 'admin'),
+(2, 'operator', 'operator', '085727747959', 'a2ac1e2a67f66b89e4b181d542c8fcca', 'operator'),
+(3, 'amien wawa', 'amien kurniawan wawa', '085727747950', 'b9d53361d514b1abb71b782bb4c4ea06', 'admin'),
+(8, 'amien', 'amien kurniawan', '098', 'b9d53361d514b1abb71b782bb4c4ea06', 'operator'),
+(10, 'theo', 'theo', '98000', '57295d54183a6a1a90a12e9979d66c9d', 'operator');
 
 -- --------------------------------------------------------
 
@@ -50,23 +54,20 @@ INSERT INTO `admin` (`id_admin`, `username`, `nama_lengkap`, `password`, `role`)
 
 CREATE TABLE `barang` (
   `id_barang` int(10) UNSIGNED NOT NULL,
-  `kode_satuan` varchar(4) NOT NULL,
+  `kode_satuan` int(4) NOT NULL,
   `nama_barang` varchar(35) NOT NULL,
-  `jenis_barang` varchar(30) NOT NULL,
-  `jumlah` int(10) NOT NULL,
-  `harga` varchar(10) NOT NULL,
-  `total_harga` varchar(10) NOT NULL,
-  `tanggal` date NOT NULL
+  `jenis_barang` int(30) NOT NULL,
+  `stock` int(10) NOT NULL,
+  `harga_barang` int(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `barang`
 --
 
-INSERT INTO `barang` (`id_barang`, `kode_satuan`, `nama_barang`, `jenis_barang`, `jumlah`, `harga`, `total_harga`, `tanggal`) VALUES
-(1, 'PAK', 'Kertas FC Polio', 'Barang-Barang Keperluan Kantor', 5, '5.000,00', '25.000,00', '2017-07-21'),
-(2, 'TUB', 'Tinta Roneo', 'Barang-Barang Keperluan Kantor', 10, '200.000,00', '2.000.000,', '2017-07-08'),
-(3, 'BH', 'Pisau Wiwil', 'Barang-Barang Keperluan Kantor', 20, '25.000,00', '500.000,00', '2017-07-11');
+INSERT INTO `barang` (`id_barang`, `kode_satuan`, `nama_barang`, `jenis_barang`, `stock`, `harga_barang`) VALUES
+(13, 2, 'amien', 1, 0, 1000),
+(14, 2, 'amien', 1, 5, 1500);
 
 -- --------------------------------------------------------
 
@@ -77,12 +78,20 @@ INSERT INTO `barang` (`id_barang`, `kode_satuan`, `nama_barang`, `jenis_barang`,
 CREATE TABLE `barang_keluar` (
   `id_keluar` int(10) NOT NULL,
   `tanggal` date NOT NULL,
-  `kode_satuan` varchar(4) NOT NULL,
+  `kode_barang` int(4) NOT NULL,
   `jumlah` int(10) NOT NULL,
-  `harga` varchar(10) NOT NULL,
-  `total_harga` varchar(10) NOT NULL,
-  `penerima` varchar(30) NOT NULL
+  `penerima` varchar(30) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `lokasi` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `barang_keluar`
+--
+
+INSERT INTO `barang_keluar` (`id_keluar`, `tanggal`, `kode_barang`, `jumlah`, `penerima`, `id_user`, `lokasi`) VALUES
+(3, '2017-12-03', 13, 10, 'amien', 8, 'jakarta'),
+(4, '2017-12-04', 14, 10, 'amien', 8, 'semarang');
 
 -- --------------------------------------------------------
 
@@ -92,19 +101,22 @@ CREATE TABLE `barang_keluar` (
 
 CREATE TABLE `barang_masuk` (
   `id_masuk` int(10) UNSIGNED NOT NULL,
+  `kode_barang` int(11) NOT NULL,
   `tanggal` date NOT NULL,
-  `kode_satuan` varchar(4) NOT NULL,
+  `nama_barang` varchar(255) NOT NULL,
+  `kode_satuan` int(4) NOT NULL,
+  `jenis_barang` int(11) NOT NULL,
   `jumlah` int(10) NOT NULL,
-  `harga` varchar(10) NOT NULL,
-  `total_harga` varchar(10) NOT NULL
+  `lokasi` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `barang_masuk`
 --
 
-INSERT INTO `barang_masuk` (`id_masuk`, `tanggal`, `kode_satuan`, `jumlah`, `harga`, `total_harga`) VALUES
-(1, '2017-07-20', 'PAK', 5, '5.000,00', '25.000,00');
+INSERT INTO `barang_masuk` (`id_masuk`, `kode_barang`, `tanggal`, `nama_barang`, `kode_satuan`, `jenis_barang`, `jumlah`, `lokasi`) VALUES
+(16, 13, '2017-12-03', 'amien', 2, 1, 10, 'semarang'),
+(17, 14, '2017-12-03', 'amien', 2, 1, 15, 'jakarta');
 
 -- --------------------------------------------------------
 
@@ -132,22 +144,39 @@ INSERT INTO `ci_sessions` (`session_id`, `ip_address`, `user_agent`, `last_activ
 -- --------------------------------------------------------
 
 --
--- Table structure for table `operator`
+-- Table structure for table `jenis_barang`
 --
 
-CREATE TABLE `operator` (
-  `id_operator` int(5) UNSIGNED NOT NULL,
-  `nama_lengkap` varchar(30) NOT NULL,
-  `no_telp/hp` varchar(20) NOT NULL,
-  `password` varchar(30) NOT NULL
+CREATE TABLE `jenis_barang` (
+  `id_jenis` int(11) NOT NULL,
+  `jenis_barang` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `operator`
+-- Dumping data for table `jenis_barang`
 --
 
-INSERT INTO `operator` (`id_operator`, `nama_lengkap`, `no_telp/hp`, `password`) VALUES
-(101, 'Handika Pratama', '085740182195', 'operator');
+INSERT INTO `jenis_barang` (`id_jenis`, `jenis_barang`) VALUES
+(1, 'Barang - Barang Keperluan Kantor'),
+(2, 'Barang - Barang Keperluan Bangunan'),
+(3, 'Barang - Barang Keperluan Instalasi'),
+(4, 'Barang - Barang Penggergajian'),
+(5, 'Barang - Barang Lain');
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `join_barang_jenis_satuan`
+--
+CREATE TABLE `join_barang_jenis_satuan` (
+`id_barang` int(10) unsigned
+,`nama_barang` varchar(35)
+,`stock` int(10)
+,`harga_barang` int(25)
+,`jenis_barang` varchar(255)
+,`kode` varchar(255)
+,`keterangan` varchar(255)
+);
 
 -- --------------------------------------------------------
 
@@ -176,6 +205,137 @@ INSERT INTO `saldo_bulanan` (`id_barang`, `kode_satuan`, `nama_barang`, `jenis_b
 (6, 'PAK', 'Kertas FC Polio', 'Barang-Barang Keperluan Kantor', 5, '5.000,00', '25.000,00', '25.000,00', '2017-07-21'),
 (7, 'PAK', 'Kertas Kertas Samak', 'Barang-Barang Keperluan Kantor', 5, '20.000,00', '100.000,00', '100.000,00', '2017-07-19'),
 (8, 'TUB', 'Tinta Roneo', 'Barang-Barang Keperluan Kantor', 10, '200.000,00', '2.000.000,', '2.000.000,', '2017-07-08');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `satuan`
+--
+
+CREATE TABLE `satuan` (
+  `id_satuan` int(11) NOT NULL,
+  `kode` varchar(255) NOT NULL,
+  `keterangan` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `satuan`
+--
+
+INSERT INTO `satuan` (`id_satuan`, `kode`, `keterangan`) VALUES
+(1, 'AR', 'Are'),
+(2, 'BH', 'Buah'),
+(3, 'BK', 'Buku'),
+(4, 'BOX', 'Box'),
+(5, 'BKS', 'Bungkus'),
+(6, 'BTG', 'Batang'),
+(7, 'BTL', 'Botol'),
+(8, 'BTR', 'Butir'),
+(9, 'CC', 'Centicubic'),
+(10, 'CM', 'Centimeter'),
+(11, 'Dos', 'Dos'),
+(12, 'DRM', 'DRUM'),
+(13, 'DSN', 'DOSIN'),
+(14, 'FT', 'Feet'),
+(15, 'GRM', 'Gram'),
+(16, 'GRS', 'GROS'),
+(17, 'HA', 'Hektoare'),
+(18, 'HM', 'Hektometer'),
+(19, 'INC', 'Inci'),
+(20, 'KG', 'Kilogram'),
+(21, 'KLG', 'Kaleng'),
+(22, 'KM', 'Kilometer'),
+(23, 'KPL', 'Kapling'),
+(24, 'KVA', 'Kilo Volt Amp'),
+(26, 'LBR', 'Lembar'),
+(27, 'LOK', 'Lokasi'),
+(28, 'LTR', 'Liter'),
+(29, 'M2', 'Meterpersegi'),
+(30, 'M3', 'Meterkubik'),
+(31, 'MGR', 'Miligram'),
+(32, 'ML', 'Meter'),
+(33, 'MLG', 'Miligram'),
+(34, 'MLL', 'Mililiter'),
+(35, 'MM', 'Milimeter'),
+(36, 'MTR', 'Meter'),
+(37, 'OBY', 'Obyek'),
+(38, 'ONS', 'ONS'),
+(39, 'PAK', 'Pak'),
+(40, 'PIL', 'Peil Isi X KG'),
+(41, 'PK', 'Power Horse'),
+(42, 'PKT', 'Paket'),
+(43, 'PSG', 'Pasang'),
+(44, 'PT', 'Potong'),
+(45, 'ROL', 'Rol'),
+(46, 'SET', 'set'),
+(47, 'STL', 'STEL'),
+(48, 'T', 'Ton'),
+(49, 'TBG', 'Tabung'),
+(50, 'TUB', 'Tube'),
+(51, 'UNT', 'Unit'),
+(52, 'V', 'Volt'),
+(53, 'WAT', 'Watt');
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `view_barang_keluar`
+--
+CREATE TABLE `view_barang_keluar` (
+`id_user` int(11)
+,`kode` varchar(255)
+,`jenis_barang` varchar(255)
+,`tanggal` date
+,`id_keluar` int(10)
+,`jumlah` int(10)
+,`penerima` varchar(30)
+,`lokasi` varchar(20)
+,`nama_barang` varchar(35)
+,`harga_barang` int(25)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `view_barang_masuk`
+--
+CREATE TABLE `view_barang_masuk` (
+`lokasi` varchar(20)
+,`id_masuk` int(10) unsigned
+,`tanggal` date
+,`kode` varchar(255)
+,`nama_barang` varchar(255)
+,`jumlah` int(10)
+,`jenis_barang` varchar(255)
+,`harga_barang` int(25)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `join_barang_jenis_satuan`
+--
+DROP TABLE IF EXISTS `join_barang_jenis_satuan`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `join_barang_jenis_satuan`  AS  select `a`.`id_barang` AS `id_barang`,`a`.`nama_barang` AS `nama_barang`,`a`.`stock` AS `stock`,`a`.`harga_barang` AS `harga_barang`,`b`.`jenis_barang` AS `jenis_barang`,`c`.`kode` AS `kode`,`c`.`keterangan` AS `keterangan` from ((`barang` `a` join `jenis_barang` `b` on((`a`.`jenis_barang` = `b`.`id_jenis`))) join `satuan` `c` on((`a`.`kode_satuan` = `c`.`id_satuan`))) ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `view_barang_keluar`
+--
+DROP TABLE IF EXISTS `view_barang_keluar`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_barang_keluar`  AS  select `d`.`id_user` AS `id_user`,`b`.`kode` AS `kode`,`c`.`jenis_barang` AS `jenis_barang`,`d`.`tanggal` AS `tanggal`,`d`.`id_keluar` AS `id_keluar`,`d`.`jumlah` AS `jumlah`,`d`.`penerima` AS `penerima`,`d`.`lokasi` AS `lokasi`,`a`.`nama_barang` AS `nama_barang`,`a`.`harga_barang` AS `harga_barang` from (((`barang` `a` join `satuan` `b` on((`b`.`id_satuan` = `a`.`kode_satuan`))) join `jenis_barang` `c` on((`c`.`id_jenis` = `a`.`jenis_barang`))) join `barang_keluar` `d` on((`d`.`kode_barang` = `a`.`id_barang`))) ;
+
+-- --------------------------------------------------------
+
+--
+-- Structure for view `view_barang_masuk`
+--
+DROP TABLE IF EXISTS `view_barang_masuk`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_barang_masuk`  AS  select `a`.`lokasi` AS `lokasi`,`a`.`id_masuk` AS `id_masuk`,`a`.`tanggal` AS `tanggal`,`c`.`kode` AS `kode`,`a`.`nama_barang` AS `nama_barang`,`a`.`jumlah` AS `jumlah`,`b`.`jenis_barang` AS `jenis_barang`,`d`.`harga_barang` AS `harga_barang` from (((`barang_masuk` `a` join `jenis_barang` `b` on((`b`.`id_jenis` = `a`.`jenis_barang`))) join `satuan` `c` on((`c`.`id_satuan` = `a`.`kode_satuan`))) join `barang` `d` on((`d`.`id_barang` = `a`.`kode_barang`))) ;
 
 --
 -- Indexes for dumped tables
@@ -206,16 +366,22 @@ ALTER TABLE `barang_masuk`
   ADD PRIMARY KEY (`id_masuk`);
 
 --
--- Indexes for table `operator`
+-- Indexes for table `jenis_barang`
 --
-ALTER TABLE `operator`
-  ADD PRIMARY KEY (`id_operator`);
+ALTER TABLE `jenis_barang`
+  ADD PRIMARY KEY (`id_jenis`);
 
 --
 -- Indexes for table `saldo_bulanan`
 --
 ALTER TABLE `saldo_bulanan`
   ADD PRIMARY KEY (`id_barang`);
+
+--
+-- Indexes for table `satuan`
+--
+ALTER TABLE `satuan`
+  ADD PRIMARY KEY (`id_satuan`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -225,32 +391,37 @@ ALTER TABLE `saldo_bulanan`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id_admin` int(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_admin` int(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT for table `barang`
 --
 ALTER TABLE `barang`
-  MODIFY `id_barang` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_barang` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 --
 -- AUTO_INCREMENT for table `barang_keluar`
 --
 ALTER TABLE `barang_keluar`
-  MODIFY `id_keluar` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_keluar` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `barang_masuk`
 --
 ALTER TABLE `barang_masuk`
-  MODIFY `id_masuk` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_masuk` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 --
--- AUTO_INCREMENT for table `operator`
+-- AUTO_INCREMENT for table `jenis_barang`
 --
-ALTER TABLE `operator`
-  MODIFY `id_operator` int(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=102;
+ALTER TABLE `jenis_barang`
+  MODIFY `id_jenis` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `saldo_bulanan`
 --
 ALTER TABLE `saldo_bulanan`
   MODIFY `id_barang` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT for table `satuan`
+--
+ALTER TABLE `satuan`
+  MODIFY `id_satuan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
